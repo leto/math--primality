8c5ec991acb18e83106f3a128a77e496d293f059
Duke Leto <jonathan@leto.net>
2009-03-30 22:46:03 -0700

    Initial skeleton from leto.net/modstarter.cgi

---
+++ lib/Math/Primality.pm
@@ -0,0 +1,107 @@
+package Math::Primality;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Math::Primality - The great new Math::Primality!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Math::Primality;
+
+    my $foo = Math::Primality->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jonathan Leto, C<< <jonathan at leto.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-math-primality at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Math::Primality>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Math::Primality
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Math::Primality>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Math::Primality>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Math::Primality>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Math::Primality>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Jonathan Leto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Math::Primality
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+	use_ok( 'Math::Primality' );
+}
+
+diag( "Testing Math::Primality $Math::Primality::VERSION, Perl $], $^X" );
